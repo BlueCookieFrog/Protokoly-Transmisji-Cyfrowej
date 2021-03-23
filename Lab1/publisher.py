305c037e917fcc7c5bd7a53072e3200251608d62
Borys Staszczak <35380525+BlueCookieFrog@users.noreply.github.com>
2021-03-23 09:17:54 +0100

    publisher & subscriber

---
+++ Lab1/publisher.py
@@ -0,0 +1,34 @@
+import sys
+import paho.mqtt.client as mqtt
+from random import randint
+
+class Publisher:
+    def __init__(self, topic_id, val_range: list) -> None:
+        self.data = val_range
+        self.broker = "127.0.0.1"
+        self.port = 1883
+        self.topic = f"topic/test/{topic_id}"
+
+
+    def connect(self) -> mqtt.Client:
+        client = mqtt.Client()
+        client.connect(self.broker, self.port)
+        return client
+
+    def _generate_data(self) -> int:
+        return randint(int(self.data[0]), int(self.data[1]))
+
+    def publish(self, client: mqtt.Client) -> None:
+        data = self._generate_data()
+        client.publish(f"{self.topic}", data)
+
+def run():
+    pub = Publisher(sys.argv[1], sys.argv[2:4])
+    client = pub.connect()
+    pub.publish(client)
+
+
+if __name__ == "__main__":
+
+    run()
+
---
+++ Lab1/subscriber.py
@@ -0,0 +1,68 @@
+import sys
+from datetime import datetime as dt
+import matplotlib.pyplot as plt
+import matplotlib.animation as Animation
+import paho.mqtt.client as mqtt
+
+
+class Subscriber:
+    def __init__(self, topic_id) -> None:
+        self.broker = "127.0.0.1"
+        self.port = 1883
+        self.topic = f"topic/test/{topic_id}"
+
+    def log(self, msg, date):
+        with open(f"logs/{msg.topic}.txt", "a") as f:
+            f.write(f"{date}: Received `{msg.payload.decode()}` from {msg.topic} topic\n")
+
+    def on_connect(client, userdata, flags, rc):
+            if rc == 0:
+                print(f"Successfully connected")
+            else:
+                print(f"Error {rc}")
+
+    def connect(self) -> mqtt.Client:
+        def on_connect(client, userdata, flags, rc):
+            if rc == 0:
+                print(f"Successfully connected")
+            else:
+                print(f"Error {rc}")
+
+        client = mqtt.Client()
+        client.on_connect = on_connect
+        client.connect(self.broker, self.port)
+        return client
+
+    def subscribe(self, client: mqtt) -> None:
+        def on_message(client, userdata, msg):
+            date = dt.now().strftime("[%d.%m.%Y, %H:%M:%S]")
+            print(f"{date}: Received `{msg.payload.decode()}` from {msg.topic} topic")
+            self.log(msg, date)
+
+        client.subscribe(self.topic)
+        client.on_message = on_message
+
+
+def menu() -> Subscriber:
+
+    topic_id = int(input("Write id of topic that you want to subscribe: "))
+
+    if topic_id >= 1:
+        return Subscriber(topic_id)
+    else:
+        raise ValueError
+
+def main():
+    try:
+        subscriber = menu()
+    except ValueError:
+        print("Topic id should be integer with values >= 1")
+        sys.exit()
+    client = subscriber.connect()
+    subscriber.subscribe(client)
+    client.loop_forever()
+
+
+if __name__ == "__main__":
+
+    main()
